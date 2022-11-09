a546f022f5cf0b05d9d61adbffec0b4068439204
Derrick Hodges <hodgesd@gmail.com>
2022-11-09 10:20:05 +0100

    add boilerplate code from video

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import Input from ". /components/Input";
+import useTemp from ". /hooks/useTemp";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+  const [temp, Dispatch] = useTemp();
+return (
+  <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <main className="bg-white p-8 rounded shadow"›
+      <h1 className="text-2xl font-bold mb-4 text-center"
+        Temperature Converter
+      </h1>
+
+      ‹div className="flex gap-x-4 justify-center"›
+        <Input id="celsius">
+
+        </Input>
+
+
+      <Input
+      id="fahrenheit">
+
+      </Input>
+
+      </div>
+    </main>
+  </div>
+);
+};
 
 export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,26 @@
+import capitalize from "lodash/capitalize";
+import PropTypes from "prop-types";
+
+export default function Input({ id, label, value, onChange }) {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={id} className="text-sm font-bold mb-2">
+        {label}
+      </label>
+      <input
+        id={id}
+        type="number"
+        value={value}
+        onChange={onChange}
+        className="border border-gray-300 rounded px-4 py-2"
+      />
+    </div>
+  );
+}
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  value: PropTypes.one0fType([PropTypes.string, PropTypes.number]).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
