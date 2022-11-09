d1741a53fbe557f2b6ec294387f4f9157826d08e
Derrick Hodges <hodgesd@gmail.com>
2022-11-09 12:45:55 +0100

    add boilerplate code from clues video

--- src/hooks/useTemp.js
+++ src/hooks/useTemp.js
@@ -1,9 +1,10 @@
 import { useReducer } from "react";
 import tempReducer from "./tempReducer";
 
-export default () = {
-const [temp, dispatch] = useReducer (tempReducer, {
-  celsius: 0,
-  fahrenheit: 32,
-});
-return [temp, dispatch];
+export default () => {
+  const [temp, dispatch] = useReducer(tempReducer, {
+    celsius: 0,
+    fahrenheit: 32,
+  });
+  return [temp, dispatch];
+};
